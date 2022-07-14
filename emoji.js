983eeccd32073c2b4230e25a21bfbc98cb05f685
Carlos Ramirez <carlosrv125@icloud.com>
2022-07-13 23:48:31 -0500

    refactor code

--- emoji.js
+++ emoji.js
@@ -1,18 +1,17 @@
 import express from 'express'
-import { pool } from './index.js'
+import { querySelectEmoji } from './queries.js'
 const router = express.Router()
 
-router.use(function timeLog(req, res, next) {
+router.use((req, res, next) => {
   console.log('Time: ', Date.now())
   next()
 })
 
-router.get('/emoji', function(req, res) {
-  pool.query('SELECT * from emojis;', function (error, results, fields) {
-    if (error) {
-      res.status(500).send(error)
-    }
+router.get('/emoji', (req, res) => {
+  querySelectEmoji((results) => {
     res.send(results)
+  }, (error) => {
+    res.status(500).send(error)
   })
 })
 
